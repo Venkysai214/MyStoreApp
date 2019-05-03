f0711099011f5b8139bc644e6222dc3dbc7401d7
TL <Admin@192.168.1.230>
2019-05-03 23:45:43 +0530

    Venky-Setters and getters created

--- MyStoreApplication/src/main/java/com/nt/dto/MyStoreDTO.java
+++ MyStoreApplication/src/main/java/com/nt/dto/MyStoreDTO.java
@@ -6,4 +6,18 @@ public class MyStoreDTO implements Serializable{
 
 	private String username;
 	private String password;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
--- MyStoreApplication/src/main/java/com/nt/vo/MyStoreVO.java
+++ MyStoreApplication/src/main/java/com/nt/vo/MyStoreVO.java
@@ -5,5 +5,19 @@ import java.io.Serializable;
 public class MyStoreVO implements Serializable{
 
 	private String username;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	private String password;
+	
+	
 }
